73ab7851f7f80082f80915e4441572f9d0388f6a
Rajesh Pillai <pillai.rajesh@gmail.com>
2012-01-02 14:39:29 +0530

    added columnresizer, basic search

--- js/jquery.zengrid.js
+++ js/jquery.zengrid.js
@@ -1,15 +1,66 @@
+/*!
+ * zengrid (jquery plugin) v0.2
+ * http://tekacademy.com
+ *
+ * Copyright 2012, Rajesh Pillai
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://tekacademy.com/zengrid/license
+ *
+ * Date: Mon Jan 01 14:40 2012
+ */
+
 
 $(function() {
   var $debug = $("#debug");
+  
+  $(window).resize(function() {
+    $(".grid").each(function() {
+      $(this).equalize();
+    });
+  });
+    
+  $.fn.search = function (inputVal)
+  {
+      var table = $(this);
+      table.find('tr').each(function(index, row)
+      {
+          console.log(index);
+          var allCells = $(row).find('td');
+          if(allCells.length > 0)
+          {
+              var found = false;
+              allCells.each(function(index, td)
+              {
+                  var regExp = new RegExp(inputVal, 'i');
+                  if(regExp.test($(td).text()))
+                  {
+                      found = true;
+                      return false;
+                  }
+              });
+              if(found == true)$(row).show();else $(row).hide();
+          }
+      });
+  };
+   
   $.fn.zenGrid = function(options) {
     var settings = $.extend( {
-      'width' : '100%',
-      'height' : '120px'
+      width : '100%',
+      height : '120px',
+      resizeColumns: true,
+      showSearch: true
     }, options);
+    
+    
     
     return this.each(function(){
       
       var $grid = $(this);
+      $grid.addClass("grid");
+      
+      if($grid.parents(".gridContainer").length) {
+          return $grid;
+      }
 
       // wrap the main grid
       $grid.wrap("<div class='gridContainer'/>");
@@ -17,59 +68,93 @@ $(function() {
       var $thead = $grid.find("thead").clone();
       
       // remove the header from the original grid
-      $("thead", $grid).remove();
+      $("thead",$grid).remove();
       
       // prepend a gridHeader before the main table
-      $(".gridContainer").prepend("<div class='gridHeader'/>");
+      $grid.closest(".gridContainer").prepend("<div class='gridHeader'/>");
       
-      $(".gridHeader").append("<table class='header'/>");
-     
+      $(".gridHeader", $grid.closest(".gridContainer")).append("<table class='header'/>");
       
       // Add the thead to the grid header table
-      $(".header").html($thead);
+      //$(".header").html($thead);
+      $grid.closest(".gridContainer").find(".header").html($thead);
       
       // wrap the main grid in its own container.
-      $(".grid").wrap("<div class='gridContent'/>");
+      $grid.wrap("<div class='gridContent'/>");
       
       // set container width
-      $(".gridContainer").width(settings.width);
-      //$(".gridContent").width(settings.width);
-      //$(".gridHeader").width(settings.width);
+      $grid.parent(".gridContainer").height(settings.height);
+      $grid.parent(".gridContent").height(settings.height);
       
-      $(".gridContainer").height(settings.height);
-      $(".gridContent").height(settings.height);
-      
-      var $gridHeader = $(".header");
-      var $gridBody = $(".grid");
-      var $gridContainer = $(".gridContainer");
+      var $gridHeader = $grid.closest(".gridContainer").find(".header"); 
+      var $gridContainer = $grid.closest(".gridContainer");
       
       // wrap each th text in div
       $gridHeader.find("th").each(function(i) {
+        
+        var $th = $(this);
         var text = $(this).text();
-        $(this).html("<div class=''>" + text + "</div>");
+        $th.html("");
+        
+        var $colHandle = $("<div class='col-handle'></div>");
+        $colHandle.height($th.outerHeight());
+        var $colResizer = $("<div class='col-resizer'></div>").html(text).append($colHandle);
+        if (settings.resizeColumns) {
+          $colHandle.mousedown(function(e) {
+              var startX = e.clientX;
+              $(document).bind("mousemove.grid", function(e) {
+                 $th.width($th.width() + (e.clientX - startX)); 
+                 startX = e.clientX;
+                 $grid.equalize();
+                
+              });
+              $(document).mouseup(function(e) {
+                $(document).unbind("mousemove.grid");
+              });
+          });
+        }
+        $th.append($colResizer);
+        
       });
       
       
       // equalize the columns
-      $gridBody.equalize();
+      $grid.equalize();
       
       // Add the title bar
       $gridContainer.prepend("<div class='title-bar'/>");
-      $(".title-bar").text(settings.caption);
       
+      var title = settings.caption;
+      
+      if ($grid.attr("title")) {
+            title = $grid.attr("title");
+      }
+      $(".title-bar", $gridContainer).text(title);
+      
+      // add search bar
+      if (settings.showSearch) {
+        $gridContainer.children(":first").append("<div class='search-bar'>Search:<input type='text' class='search-input' /></div>");
+           $('input.search-input').keyup(function() {
+               $grid.search($(this).val());
+          });
+      }
     });
+  };
+
+  $.fn.setColumnResizable = function() {
+    
   };
   
   $.fn.equalize = function() {
-   // Get the first row in the grid
+   
     var $firstRow = $(this).first("tr");
-    var $gridHeader = $(".header"); 
+    var $gridHeader = $(this).closest(".gridContainer").find(".header"); 
     var $gridBody = $(this);
     
     // if there is a scrollbar account for it
     if(($gridBody.height() > $gridBody.parents(".gridContent").height())) {
-        $gridHeader.find(".scrollTh").remove();
-        var $newTh = $("<th></th>").addClass("scrollTh").css({
+        $gridHeader.find(".scroll").remove();
+        var $newTh = $("<th></th>").addClass("scroll").css({
                 padding:0,
                 margin:0,
                 width:15
@@ -78,7 +163,7 @@ $(function() {
     } 
     else if ($gridBody.parents(".gridWrapper").height() > $gridBody.height()) 
     {
-        $(".scrollTh").remove();
+        $(".scroll").remove();
     }
     
     // set header width based on user configuration in the html
@@ -88,23 +173,32 @@ $(function() {
       if (w !== undefined) {
         $hd.width(w);
       }
-      
     });
     
     // Size the body row width to header row
     $gridBody.find("tr:first td:visible").each(function(i) {
+      
        $(this).width($gridHeader.find("th").eq(i).width());
     });
   };
 });
 
 $(function() {
-  $(".grid").zenGrid(
+  $(".grid1").zenGrid(
     {
       width: "300px",
       height: "120px",
       caption: "Zen Grid"
     }
   );
+  
+   $(".grid2").zenGrid(
+    {
+      width: "300px",
+      height: "120px",
+      caption: "Zen Grid",
+      resizeColumns: false,
+      showSearch: false
+    }
+  );
 });
-
